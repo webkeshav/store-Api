4d0a354e8a9c32b74f29ccb7bbef9265c894c440
Keshav <keshavtomar999@gmail.com>
2024-06-02 00:50:14 +0530

    numeric filters

--- controllers/products.js
+++ controllers/products.js
@@ -3,12 +3,12 @@ const Product = require('../models/product')
 const getAllProductsStatic = async (req,res)=>{
 const products = await Product.find({
     featured:true
-})
+}).select('name price')
     res.status(200).json({products,nbHits:products.length})
 }
 
 const getAllProducts = async (req,res)=>{
-    const {featured,company,name,sort} = req.query
+    const {featured,company,name,sort,fields ,numericFilters } = req.query
     const queryObject = {}
 
     if(featured){
@@ -21,13 +21,46 @@ const getAllProducts = async (req,res)=>{
         queryObject.name = {$regex: name,$options:'i'}
     }
 
+    if(numericFilters){
+        const operatorMap = {
+            '>' : '$gt',
+            '>=' : '$gte',
+            '=' : 'eq',
+            '<' : '$lt',
+            '<=' : '$lte'
+        }
+        const regex = /\b(>|>=|=|<|<=)\b/g
+        let filters = numericFilters.replace(regex,(match)=> `-${operatorMap[match]}-`)
+        const options = ['price','rating'] // this method can only be implemented on numeric value fields
+        filters = filters.split(',').forEach((item) => {
+            const [field,operator,value] = item.split('-')
+            if(options.includes(field)){
+                queryObject[field] = {[operator] : Number(value)}
+            }
+            
+        });        console.log(filters)
+    }
+
     let result =  Product.find(queryObject)
+    //sort
     if(sort){
         const sortList = sort.split(',').join(' ')
         result = result.sort(sortList)
     }else{
         result = result.sort('createAt')
     }
+
+    if(fields){
+        const fieldsList = fields.split(',').join(' ')
+        result = result.select(fieldsList)
+    }
+
+
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page-1)*limit
+
+    result = result.skip(skip).limit(limit)
     const products = await result
     res.status(200).json({products,nbHits: products.length})
 }
